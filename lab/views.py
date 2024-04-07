f121cd5a0dad58975c3af876ae7daf5b21200952
joisemp <joise1124@gmail.com>
2024-04-07 10:16:49 +0530

    Created LabListView - Created urls.py in lab app - Added view to urls.py file - Added lab urls to main urls - Created template labs.html for lab list view

--- lab/views.py
+++ lab/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from . models import Item, Lab
+from . utils import get_total_item_qty
 
-# Create your views here.
+
+class LabListView(generic.ListView):
+    template_name = "lab/labs.html"
+    queryset = Lab.objects.all()
+    context_object_name = 'labs'
+    ordering = ['-id']
