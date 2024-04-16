5d4197d21f2efab2b309c41d1aea0d2d0f87f03c
joisemp <joise1124@gmail.com>
2024-04-17 00:16:32 +0530

    Implemented account activation feature on registration - Added view - Added Url - Created account_activation_email.py - Created custom token generator - Created email template

--- core/views.py
+++ core/views.py
@@ -1,5 +1,3 @@
-from django.forms import BaseModelForm
-from django.http import HttpResponse
 from django.urls import reverse, reverse_lazy
 from . utils import generate_password
 from . forms import CustomAuthenticationForm, UserRegisterForm
@@ -10,6 +8,11 @@ from django.shortcuts import redirect
 from . models import User
 from lab.mixins import RedirectLoggedInUserMixin, AdminOnlyAccessMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .account_activation_email import send_account_activation_mail
+from . token_generator import account_activation_token
+from django.utils.http import urlsafe_base64_decode
+from django.http import HttpResponse
+from django.utils.encoding import force_str
 
 
 class LandingPageView(RedirectLoggedInUserMixin, generic.TemplateView):
@@ -23,7 +26,9 @@ class UserRegisterView(generic.CreateView):
     def form_valid(self, form):
         user = form.save(commit=False)
         user.is_admin = True
+        user.is_active = False
         user.save()
+        send_account_activation_mail(self.request, user, form)
         return super(UserRegisterView, self).form_valid(form)  
     
     def get_success_url(self):
@@ -88,4 +93,20 @@ class AddUserView(LoginRequiredMixin, AdminOnlyAccessMixin, generic.CreateView):
             is_staff=True
         )
         return redirect('lab:lab-list')
-        
+          
+
+class ActivateAccountView(generic.View):
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        else:
+            return HttpResponse('Activation link is invalid!')
+
