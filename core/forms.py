d696672cf70e498c6125dadbbe66375b5a69081e
joisemp <joise1124@gmail.com>
2024-04-16 23:20:09 +0530

    Added UserRegisterView - Created form - Added url - Added template

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,19 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+
+User = get_user_model()
+
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={'required': True}))
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'required': True}))
+    
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
