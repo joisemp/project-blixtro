a8126a63398ea1cf35b1d4dbff4a6f023c6be4fb
joisemp <joise1124@gmail.com>
2024-04-28 12:08:59 +0530

    Updated few urls for itemgroup and item

--- lab/urls.py
+++ lab/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
     path('<int:pk>/update/', views.UpdateLabView.as_view(), name='lab-update'),
     path('<int:pk>/delete/', views.DeleteLabView.as_view(), name='lab-delete'),
     
-    path('<int:pk>/<int:itemgroup>/add-item/', views.CreateItemView.as_view(), name='add-item'),
-    path('<int:pk>/<int:itemgroup>/<int:item_id>/update/', views.ItemUpdateView.as_view(), name='item-update'),
-    path('<int:pk>/<int:itemgroup>/<int:item_id>/delete/', views.ItemDeleteView.as_view(), name='item-delete'),
+    path('<int:pk>/add-item/', views.CreateItemView.as_view(), name='add-item'),
+    path('<int:pk>/<int:item_id>/update/', views.ItemUpdateView.as_view(), name='item-update'),
+    path('<int:pk>/<int:item_id>/delete/', views.ItemDeleteView.as_view(), name='item-delete'),
     
-    path('<int:pk>/create-group/', views.ItemGroupCreateView.as_view(), name='create-group'),
+    path('<int:pk>/groups/create/', views.ItemGroupCreateView.as_view(), name='create-group'),
     path('<int:pk>/groups/', views.ItemGroupListView.as_view(), name='group-list'),
     path('<int:pk>/groups/<int:itemgroup>/', views.ItemGroupDetailView.as_view(), name='group-detail'),
     path('<int:pk>/groups/<int:itemgroup>/delete/', views.ItemGroupDeleteView.as_view(), name='group-delete'),
