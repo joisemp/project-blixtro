6a2fc35e388710c90d0b996a0d0dfc28f36402a2
joisemp <joise1124@gmail.com>
2024-08-15 12:31:39 +0530

    Created Vendor Create feature

--- src/apps/org/urls.py
+++ src/apps/org/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from apps.org import views
+from apps.purchases import views as purchase_views
 
 app_name = 'org'
 
@@ -10,6 +11,8 @@ urlpatterns = [
     path('<int:org_id>/depts/<int:dept_id>/update/', views.DepartmentUpdateView.as_view(), name='dept-update'),
     path('<int:org_id>/depts/<int:dept_id>/delete/', views.DepartmentDeleteView.as_view(), name='dept-delete'),
     # path('<int:org_id>/generate-report/', core_views.GenerateReportView.as_view(), name='generate-report'),
+    
+    path('<int:org_id>/vendors/add/', purchase_views.VendorCreateView.as_view(), name='vendor-create'),
     
     path('<int:org_id>/depts/<int:dept_id>/lab/', include('apps.lab.urls', namespace='lab')),
 ]
--- src/apps/purchases/models.py
+++ src/apps/purchases/models.py
@@ -8,6 +8,9 @@ class Vendor(models.Model):
     name = models.CharField(max_length=255)
     address = models.TextField()
     
+    def __str__(self):
+        return f"{self.name}"
+    
 
 class Purchase(models.Model):
     org = models.ForeignKey(Org, on_delete=models.CASCADE)
