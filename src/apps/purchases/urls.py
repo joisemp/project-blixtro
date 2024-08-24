cf1d9f6505d56432c543265a7e768547735d4f19
joisemp <joise1124@gmail.com>
2024-08-25 00:32:05 +0530

    Created PurchaseCompleteView

--- src/apps/purchases/urls.py
+++ src/apps/purchases/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('<int:purchase_id>/', views.PurchaseDetailView.as_view(), name='purchase-detail'),
     path('<int:purchase_id>/update/', views.PurchaseUpdateView.as_view(), name='purchase-update'),
     path('<int:purchase_id>/delete/', views.PurchaseDeleteView.as_view(), name='purchase-delete'),
+    path('<int:purchase_id>/complete/', views.PurchaseCompleteView.as_view(), name='purchase-complete'),
 ]
 
--- src/apps/purchases/views.py
+++ src/apps/purchases/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.http.response import HttpResponse as HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
@@ -107,6 +108,31 @@ class PurchaseDeleteView(generic.View):
             'dept_id':self.kwargs["dept_id"], 
             'lab_id':self.kwargs["lab_id"]
             }))
+
+class PurchaseCompleteView(generic.View):
+    def get(self, request, *args, **kwargs):
+        purchase = get_object_or_404(Purchase, pk=self.kwargs["purchase_id"])
+        
+        try:
+            with transaction.atomic():
+                purchase.completed = True
+                purchase.approved = True
+                purchase.save()
+        except Exception as e:
+            return HttpResponse("An error occurred while completing the purchase.", status=500)
+
+        org_id = request.user.profile.org.pk
+        return HttpResponseRedirect(
+            reverse(
+                'org:lab:purchases:purchase-detail', 
+                kwargs={
+                    'org_id': org_id,
+                    'dept_id':purchase.lab.dept.pk,
+                    'lab_id': purchase.lab.pk,
+                    'purchase_id': purchase.pk
+                }
+            )
+        )
 
 
 class VendorCreateView(generic.CreateView):
