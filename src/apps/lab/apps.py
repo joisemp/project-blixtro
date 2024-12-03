2584a3f3c4a6aa303293820e5c2850dcb6002e45
joisemp <joise1124@gmail.com>
2024-12-03 14:37:38 +0530

    Start of v2

--- src/apps/lab/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class LabConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.lab'
-
-    def ready(self):
-        import apps.lab.signals
