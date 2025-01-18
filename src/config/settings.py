7604d8228edb01ec455b1beda56ea27dc0ba7912
joisemp <joise1124@gmail.com>
2025-01-18 23:22:14 +0530

    Dockerized the project

--- src/config/settings.py
+++ src/config/settings.py
@@ -66,34 +66,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DB_USERNAME=os.environ.get("POSTGRES_USER")
-DB_PASSWORD=os.environ.get("POSTGRES_PASSWORD")
-DB_DATABASE=os.environ.get("POSTGRES_DB")
-DB_HOST=os.environ.get("POSTGRES_HOST")
-DB_PORT=os.environ.get("POSTGRES_PORT")
-DB_IS_AVAIL = all([
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_DATABASE,
-    DB_HOST,
-    DB_PORT
-])
-
-if DB_IS_AVAIL:
-    DATABASES = {
-        "default":{
-            "ENGINE":"django.db.backends.postgresql",
-            "NAME":DB_DATABASE,
-            "USER":DB_USERNAME,
-            "PASSWORD":DB_PASSWORD,
-            "HOST":DB_HOST,
-            "PORT":DB_PORT
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'postgres',
+            'PORT': 5432,
         }
     }
 
