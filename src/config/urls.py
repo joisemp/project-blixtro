f7482ff5e649e502a197bc42e4b33d025f18fd08
joisemp <joise1124@gmail.com>
2025-02-06 00:06:32 +0530

    renamed super_admin to central_admin

--- src/config/urls.py
+++ src/config/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name="landing_page"),
     path('core/', include('core.urls', namespace='core')),
-    path('super_admin/', include('inventory.urls.super_admin', namespace='super_admin')),
+    path('central_admin/', include('inventory.urls.central_admin', namespace='central_admin')),
     path('sub_admin/', include('inventory.urls.sub_admin', namespace='sub_admin')),
     path('room_incharge/', include('inventory.urls.room_incharge', namespace='room_incharge')),
 ]
