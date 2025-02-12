920b29390eb3fd1c419dc26db85fe04fc5063bc0
sambrama-M <darklordryuk@gmail.com>
2025-02-12 14:41:14 +0530

    Added Organisation Models

--- src/inventory/models.py
+++ src/inventory/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class CentralAdmin:
-    pass
+class Organisation(models.Model):
+    organisation_name = models.CharField(max_length=255)
+    locality = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True)
+
+
