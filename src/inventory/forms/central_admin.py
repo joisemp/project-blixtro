f2aa53892bee20e53a67126ef95abd3200d88583
sambrama-M <darklordryuk@gmail.com>
2025-02-13 14:39:14 +0530

    Added Room Form

--- src/inventory/forms/central_admin.py
+++ src/inventory/forms/central_admin.py
@@ -1,8 +1,16 @@
 from django import forms
-from inventory.models import Department,
+from inventory.models import Department,Rooms
 
 class DepartmentForm(forms.ModelForm):
 
     class Meta:
         model = Department
-        fields = ["department_name"]
+        fields = ['department_name']
+
+
+class RoomForm(forms.ModelForm):
+
+    class Meata:
+        model = Rooms
+        fields = ['label','room_name','incharge']
+
