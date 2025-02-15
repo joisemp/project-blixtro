df1d90bb55d589cde768d1f26e9897fccd527c91
joisemp <joise1124@gmail.com>
2025-02-15 23:08:25 +0530

    Added list view for people

--- src/inventory/views/central_admin.py
+++ src/inventory/views/central_admin.py
@@ -1,4 +1,5 @@
 from django.views.generic.base import TemplateView
+from core.models import UserProfile
 
 class DashboardView(TemplateView):
     template_name = 'central_admin/dashboard.html'
@@ -8,31 +9,38 @@ class DashboardView(TemplateView):
         return context
 
 class PeopleListView(TemplateView):
-    template_name = 'central_admin/peoplelist.html'
-
-    def get_context_data(self, **kwargs):
-        return super().get_context_data(**kwargs)
+    template_name = 'central_admin/people_list.html'
+    model = UserProfile
+    context_object_name = 'people'
+    
+    def get_qeryset(self):
+        # Filter the queryset based on the current user organisation
+        return self.model.objects.all()
+    
 
 class RoomListView(TemplateView):
-    template_name = 'central_admin/room.html'
+    template_name = 'central_admin/room_list.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
     
+    
 class VendorListView(TemplateView):
-    template_name = 'central_admin/vendor.html'
+    template_name = 'central_admin/vendor_list.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
+
 
 class PurchaseListView(TemplateView):
-    template_name = 'central_admin/purchase.html'
+    template_name = 'central_admin/purchase_list.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
 
+
 class IssueListView(TemplateView):
-    template_name = 'central_admin/issue.html'
+    template_name = 'central_admin/issue_list.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
