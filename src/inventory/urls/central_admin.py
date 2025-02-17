ed1925ac8662ac002cbd6836f3c56d19ef5ce956
joisemp <joise1124@gmail.com>
2025-02-18 00:11:13 +0530

    Added designed template for room list page

--- src/inventory/urls/central_admin.py
+++ src/inventory/urls/central_admin.py
@@ -5,9 +5,9 @@ app_name = 'central_admin'
 
 urlpatterns = [
     path('', central_admin.DashboardView.as_view(), name='dashboard'),
-    path('people_list/', central_admin.PeopleListView.as_view(), name='people_list'),
-    path('room_list/', central_admin.RoomListView.as_view(), name='room_list'),
-    path('vendor_list/', central_admin.VendorListView.as_view(), name='vendor_list'),
-    path('purchase_list/', central_admin.PurchaseListView.as_view(), name='purchase_list'),
-    path('issue_list/', central_admin.IssueListView.as_view(), name='issue_list'),
+    path('people/', central_admin.PeopleListView.as_view(), name='people_list'),
+    path('rooms/', central_admin.RoomListView.as_view(), name='room_list'),
+    path('vendors/', central_admin.VendorListView.as_view(), name='vendor_list'),
+    path('purchases/', central_admin.PurchaseListView.as_view(), name='purchase_list'),
+    path('issues/', central_admin.IssueListView.as_view(), name='issue_list'),
 ]
--- src/inventory/views/central_admin.py
+++ src/inventory/views/central_admin.py
@@ -25,7 +25,7 @@ class RoomListView(ListView):
     context_object_name = 'rooms'
 
     def get_queryset(self):
-        return super().get_queryset().filter(organisation=self.request.user.organisation)
+        return super().get_queryset().filter(organisation=self.request.user.profile.org)
     
     
 class VendorListView(ListView):
