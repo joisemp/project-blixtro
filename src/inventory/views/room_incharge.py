0623c1410e6ecfc20d119b525eceb7421cd279e2
joisemp <joise1124@gmail.com>
2025-02-19 00:16:41 +0530

    Added item list page

--- src/inventory/views/room_incharge.py
+++ src/inventory/views/room_incharge.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, UpdateView, DeleteView, TemplateView, CreateView
-from inventory.models import Category, Room, Brand
+from inventory.models import Category, Room, Brand, Item
 from inventory.forms.room_incharge import CategoryForm, BrandForm
 
 class CategoryListView(ListView):
@@ -152,3 +152,18 @@ class BrandDeleteView(DeleteView):
     def get_queryset(self):
         room_slug = self.kwargs['room_slug']
         return super().get_queryset().filter(room__slug=room_slug, organisation=self.request.user.profile.org)
+
+
+class ItemListView(ListView):
+    template_name = 'room_incharge/item_list.html'
+    model = Item
+    context_object_name = 'items'
+
+    def get_queryset(self):
+        room_slug = self.kwargs['room_slug']
+        return super().get_queryset().filter(room__slug=room_slug, organisation=self.request.user.profile.org)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['room_slug'] = self.kwargs['room_slug']
+        return context
