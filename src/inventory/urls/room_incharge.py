cf3b2d67fe238361d949dcec5ef74dad8d8a6fab
joisemp <joise1124@gmail.com>
2025-02-19 00:02:22 +0530

    Added category list, update and delete page

--- src/inventory/urls/room_incharge.py
+++ src/inventory/urls/room_incharge.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from inventory.views import room_incharge
 
 app_name = 'room_incharge'
 
-urlpatterns = []
+urlpatterns = [
+    path('rooms/<slug:room_slug>/categories/', room_incharge.CategoryListView.as_view(), name='category_list'),
+    path('rooms/<slug:room_slug>/categories/<slug:category_slug>/update/', room_incharge.CategoryUpdateView.as_view(), name='category_update'),
+    path('rooms/<slug:room_slug>/categories/<slug:category_slug>/delete/', room_incharge.CategoryDeleteView.as_view(), name='category_delete'),
+    path('rooms/<slug:room_slug>/dashboard/', room_incharge.RoomDashboardView.as_view(), name='room_dashboard'),
+]
---
+++ src/inventory/views/room_incharge.py
@@ -0,0 +1,62 @@
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView, TemplateView
+from inventory.models import Category, Room
+from inventory.forms.room_incharge import CategoryForm
+
+class CategoryListView(ListView):
+    template_name = 'room_incharge/category_list.html'
+    model = Category
+    context_object_name = 'categories'
+
+    def get_queryset(self):
+        room_slug = self.kwargs['room_slug']
+        return super().get_queryset().filter(room__slug=room_slug, organisation=self.request.user.profile.org)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['room_slug'] = self.kwargs['room_slug']
+        return context
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    template_name = 'room_incharge/category_update.html'
+    form_class = CategoryForm
+    success_url = reverse_lazy('room_incharge:category_list')
+    slug_field = 'slug'
+    slug_url_kwarg = 'category_slug'
+
+    def get_success_url(self):
+        return reverse_lazy('room_incharge:category_list', kwargs={'room_slug': self.kwargs['room_slug']})
+
+    def form_valid(self, form):
+        category = form.save(commit=False)
+        category.organisation = self.request.user.profile.org
+        category.room = Room.objects.get(slug=self.kwargs['room_slug'])
+        category.save()
+        return redirect(self.get_success_url())
+
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'room_incharge/category_delete_confirm.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'category_slug'
+
+    def get_success_url(self):
+        return reverse_lazy('room_incharge:category_list', kwargs={'room_slug': self.kwargs['room_slug']})
+
+    def get_queryset(self):
+        room_slug = self.kwargs['room_slug']
+        return super().get_queryset().filter(room__slug=room_slug, organisation=self.request.user.profile.org)
+
+
+class RoomDashboardView(TemplateView):
+    template_name = 'room_incharge/room_dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        room_slug = self.kwargs['room_slug']
+        context['room'] = Room.objects.get(slug=room_slug)
+        return context
