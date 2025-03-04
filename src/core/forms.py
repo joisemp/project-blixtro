e231990d959294466d80f92725a575e9a1bc4c07
Nage-Sh <darklordryuk@gmail.com>
2025-03-04 14:43:46 +0530

    Added the styles for  Registration page

--- src/core/forms.py
+++ src/core/forms.py
@@ -21,7 +21,7 @@ class CustomAuthenticationForm(form_mixin.BootstrapFormMixin, AuthenticationForm
         self.fields['password'].widget.attrs.update({'class': 'form-control'})
         
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(form_mixin.BootstrapFormMixin, UserCreationForm):
     org_name = forms.CharField(max_length=200, required=True, label='Organisation name')
     first_name = forms.CharField(max_length=200, required=True)
     last_name = forms.CharField(max_length=200, required=True)
